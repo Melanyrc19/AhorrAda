9972cdeacb2a7b02b76645c03d9537a1b7bc7aea
MelanyRodriguez <melanyrc.19@gmail.com>
2025-02-10 17:25:49 -0300

    demas botones con display

--- index.js
+++ index.js
@@ -16,13 +16,35 @@ function $(selector) {
  const $selectBalance = $("#balance");
  const $selectFiltros = $("#filtros");
  const $selectOperaciones= $("#operaciones");
- const $selectCategorias= $("#categorias");
+ const $selectCategorias = $("#categorias");
 
 
 
- $buttonOperaciones.addEventListener("click", () =>{
-    $modal.style.display = "block";
- })
+  $buttonOperaciones.addEventListener("click", () =>{
+  $modal.style.display = "block";
+  $selectBalance.style.display= "none";
+  $selectFiltros.style.display = "none";
+  $selectOperaciones.style.display = "none";
+  $selectCategorias.style.display = "none";
+  })
+
+  $buttonBalance.addEventListener("click", () =>{
+    $selectBalance.style.display= "block";
+    $selectFiltros.style.display = "block";
+    $selectOperaciones.style.display = "block";
+    $selectCategorias.style.display = "none";
+    $modal.style.display = "none";
+  });
+
+  $buttonCategorias.addEventListener("click", () =>{
+    $selectBalance.style.display= "none";
+    $selectFiltros.style.display = "none";
+    $selectOperaciones.style.display = "none";
+    $selectCategorias.style.display = "block";
+    $modal.style.display = "none";
+  })
+
+
 
 
 
