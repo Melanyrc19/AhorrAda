9df42336f3b8d559a3f0081a74104d743b0c9725
Giselle <gisellesol18@gmail.com>
2025-02-13 22:06:40 -0300

    localstorage operaciones

--- index.js
+++ index.js
@@ -8,20 +8,47 @@ function $$(selector) {
 //variables
 const $buttonBalance = $("#botonBalance");
 const $botonAgregar = $("#botonAgregar");
+const $formOperacion = $("#formOperacion");
 
 //section 
 const $sectionBalance = $("#balance");
-const $sectionOperacion = $("#sectionOperacion");
-const $form = $("#form");
+const $sectionOperacion = $("#operacion");
+const $sectionCategoria = $("#categoria")
 
 //Evento 
 $buttonBalance.addEventListener("click", () => {
   $sectionBalance.classList.remove("hidden");
+  $sectionOperacion.classList.add("hidden")
 });
-$buttonOperaciones.addEventListener ("click",() =>{
+$botonAgregar.addEventListener ("click",() =>{
   $sectionOperacion.classList.remove("hidden") 
+  $sectionBalance.classList.add("hidden")
 })
 
 $botonAgregar.addEventListener('click',()=>{
   $sectionOperacion.classList.remove("hidden")
 })
+
+//Operaciones
+const getOperaciones = () => JSON.parse(localStorage.getItem("operaciones")) || [];
+const setOperaciones = operaciones => localStorage.setItem("operaciones",JSON.stringify(operaciones));
+const addOperacion = operacion =>{
+  setOperaciones([...getOperaciones(),operacion])
+}
+
+
+
+
+$formOperacion.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  const operacion = {
+    descripcion: event.target.querySelector("[name='descripcion']").value
+  }
+ 
+  addOperacion(operacion)
+})
+//objeto//
+const formOperacion ={
+  Descripcion:""
+  
+}
