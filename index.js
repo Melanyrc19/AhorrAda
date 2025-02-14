836d6bcd986640291277167381d7a6508f35e5bc
Giselle <gisellesol18@gmail.com>
2025-02-13 21:03:11 -0300

    modificaciones

--- index.js
+++ index.js
@@ -1,99 +1,27 @@
 function $(selector) {
-    return document.querySelector(selector);
-  }
-  
-  function $$(selector) {
-    return document.querySelectorAll(selector);
-  }
+  return document.querySelector(selector);
+}
 
-  const $buttonBalance = $("#botonBalance");
-  const $buttonCategorias = $("#botonCategorias");
-  const $buttonReportes= $("#botonReportes");
-  const $buttonOperaciones= $("#botonOperaciones");
-  
-  const $modal = $("#contein-modal");
+function $$(selector) {
+  return document.querySelectorAll(selector);
+}
+//variables
+const $buttonBalance = $("#botonBalance");
+const $botonAgregar = $("#botonAgregar");
 
- const $selectBalance = $("#balance");
- const $selectFiltros = $("#filtros");
- const $selectOperaciones= $("#operaciones");
- const $selectCategorias = $("#categorias");
- const $selectContenidoOperacion = $("#contenidoOperacion")
+//section 
+const $sectionBalance = $("#balance");
+const $sectionOperacion = $("#sectionOperacion");
+const $form = $("#form");
 
- const $form = $("#form")
+//Evento 
+$buttonBalance.addEventListener("click", () => {
+  $sectionBalance.classList.remove("hidden");
+});
+$buttonOperaciones.addEventListener ("click",() =>{
+  $sectionOperacion.classList.remove("hidden") 
+})
 
-
-
-  $buttonOperaciones.addEventListener("click", () =>{
-  $modal.style.display = "block";
-  $selectBalance.style.display= "none";
-  $selectFiltros.style.display = "none";
-  $selectOperaciones.style.display = "none";
-  $selectCategorias.style.display = "none";
-  })
-
-  $buttonBalance.addEventListener("click", () =>{
-    $selectBalance.style.display= "block";
-    $selectFiltros.style.display = "block";
-    $selectOperaciones.style.display = "block";
-    $selectCategorias.style.display = "none";
-    $modal.style.display = "none";
-  });
-
-  $buttonCategorias.addEventListener("click", () =>{
-    $selectBalance.style.display= "none";
-    $selectFiltros.style.display = "none";
-    $selectOperaciones.style.display = "none";
-    $selectCategorias.style.display = "block";
-    $modal.style.display = "none";
-  })
-
-  // function pintarDatos (arrayOPeraciones){
-  //   $selectContenidoOperacion.innetHTML = "";
-
-  //   for (const operacion of arrayOperaciones) {
-      
-  //     const divOperacion = document.createElement ("div");
-  //     divOperacion.classList.add("operacion", "flex", "flex-row", "justify-between", "my-4", "p-2", "border-b");
-    
-      
-
-  //   }
-
-
-  // }
-  let datosOperaciones = [];
-  $form.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    const descripcion = e.target [0].value;
-    const categoria= e.target [1].value;
-    const fecha = e.target [2].value;
-    const monto = e.target [3].value;
-    const accion = e.target [4].value;
-
-    datosOperaciones.push({
-      descripcion: descripcion,
-      categoria: categoria,
-      fecha: fecha,
-      monto: monto,
-  })
-
-
-  })
-
-
-
-  window.onload = () => {
-    datosOperaciones = JSON.parse (localStorage.getItem ("operacionese"))
-    if(datosOperaciones) {
-      pintarDatos (datosOperaciones)
-
-    } else{
-      datosOperaciones = [];
-      pintarDatos (datosOperaciones)
-
-    }
-  }
-
-
-
+$botonAgregar.addEventListener('click',()=>{
+  $sectionOperacion.classList.remove("hidden")
+})
