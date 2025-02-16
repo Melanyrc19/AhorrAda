8aecde748cdb899206ece5e7cd2f2db4c02af530
Giselle <gisellesol18@gmail.com>
2025-02-16 18:33:54 -0300

    boton de agregado de categoria

--- index.js
+++ index.js
@@ -6,87 +6,112 @@ function $$(selector) {
   return document.querySelectorAll(selector);
 }
 //variables
-const $menuHamburguesa = $("#menuHamburguesa")
-const $botonesMenu = $("#botonesMenu")
-
+const $menuHamburguesa = $("#menuHamburguesa");
+const $botonesMenu = $("#botonesMenu");
 
 const $buttonBalance = $("#botonBalance");
 const $botonAgregar = $("#botonAgregar");
 const $formOperacion = $("#formOperacion");
 const $botonOPeracion = $("#btnOperacion");
 
-const $formAñadirCategoria = $("#añadirCategoria")
-const $botonAñadirCategoria = $("botonAñadirCategoria")
+const $añadirCategoria = $("#añadirCategoria");
+const $botonAñadirCategoria = $("#botonAñadirCategoria");
 const $botonCategorias = $("#botonCategorias");
-const $inputListadosDeCategorias = $("#listadoDeCategorias")
+const $inputListadosDeCategorias = $("#listadoDeCategorias");
 
-//section 
+
+
+//section
 const $sectionBalance = $("#balance");
 const $sectionOperacion = $("#operacion");
-const $sectionCategoria = $("#categorias")
-const $contenidoOPeraciones = $("#contenidoOperaciones")
+const $sectionCategoria = $("#categorias");
+const $contenidoOPeraciones = $("#contenidoOperaciones");
 
-
-//Evento 
+//Evento
 $buttonBalance.addEventListener("click", () => {
   $sectionBalance.classList.remove("hidden");
-  $sectionOperacion.classList.add("hidden")
-  $sectionCategoria.classList.add("hidden")
+  $sectionOperacion.classList.add("hidden");
+  $sectionCategoria.classList.add("hidden");
 });
-$botonAgregar.addEventListener ("click",() =>{
-  $sectionOperacion.classList.remove("hidden") 
-  $sectionBalance.classList.add("hidden")
-  $sectionCategoria.classList.add("hidden")
-})
+$botonAgregar.addEventListener("click", () => {
+  $sectionOperacion.classList.remove("hidden");
+  $sectionBalance.classList.add("hidden");
+  $sectionCategoria.classList.add("hidden");
+});
 
-$botonOPeracion.addEventListener('click',()=>{
-  $sectionOperacion.classList.add("hidden")
+$botonOPeracion.addEventListener("click", () => {
+  $sectionOperacion.classList.add("hidden");
   $sectionBalance.classList.remove("hidden");
-  $sectionCategoria.classList.add("hidden")
-})
-$botonCategorias.addEventListener('click',()=>{
-  $sectionOperacion.classList.add("hidden")
+  $sectionCategoria.classList.add("hidden");
+});
+
+$botonCategorias.addEventListener("click", () => {
+  $sectionOperacion.classList.add("hidden");
   $sectionBalance.classList.add("hidden");
   $sectionCategoria.classList.remove("hidden");
+});
+
+
+
+//obtener elemento dom categoria
+const selectCategorias = document.querySelector("#añadirCategoria");
+//Agregar categoria
+$botonAñadirCategoria.addEventListener("click", () => {
+  const categoria = $añadirCategoria.value;
+  $añadirCategoria.value = "";
+  addCategoria(categoria);
+  listadoDeCategorias.innerHTML=""
 })
+
+ // Agregar las opciones al select
+//   categorias.forEach(categoria => {
+//    const option = document.createElement("option");
+//     option.value = categoria;
+//     option.textContent = categoria;
+//     selectCategorias.appendChild(option);
+//   });
+//  };
+
+
+
+
+
 
 // $menuHamburguesa.addEventListener('click',()=>{       hay que agregar que funcione el menu de hamburgesas en mobile y table
 //   $botonesMenu.classList.add = "block";
 //   $botonesMenu.classList.add = "flex";
 // })
 
-
-
 //Operaciones
-const getOperaciones = () => JSON.parse(localStorage.getItem("operaciones")) || [];
-const setOperaciones = operaciones => localStorage.setItem("operaciones",JSON.stringify(operaciones));
-const addOperacion = operacion =>{
-  setOperaciones([...getOperaciones(),operacion])
-}
-
-
+const getOperaciones = () =>
+  JSON.parse(localStorage.getItem("operaciones")) || [];
+const setOperaciones = (operaciones) =>
+  localStorage.setItem("operaciones", JSON.stringify(operaciones));
+const addOperacion = (operacion) => {
+  setOperaciones([...getOperaciones(), operacion]);
+};
 
 // funciones para  guardar datos de operacion:
-$formOperacion.addEventListener('submit',(event)=>{
+$formOperacion.addEventListener("submit", (event) => {
   event.preventDefault();
   const operacion = {
     descripcion: event.target.querySelector("[name='descripcion']").value,
-    monto:  event.target.querySelector("[name='monto']").value,
-    tipo:  event.target.querySelector("[name='tipo']").value,
-    categoria:  event.target.querySelector("[name='categoria']").value,
-    fecha:  event.target.querySelector("[name='fecha']").value,
-  }
+    monto: event.target.querySelector("[name='monto']").value,
+    tipo: event.target.querySelector("[name='tipo']").value,
+    categoria: event.target.querySelector("[name='categoria']").value,
+    fecha: event.target.querySelector("[name='fecha']").value,
+  };
   console.log(operacion);
- 
-  addOperacion(operacion)
-  
-  mostrarOperaciones(); 
-})
+
+  addOperacion(operacion);
+
+  mostrarOperaciones();
+});
 //funcion para que se pinte:
 
 const mostrarOperaciones = () => {
   const operaciones = getOperaciones();
-  
+
   // Primero, agregamos los títulos de las columnas (solo una vez)
   let contenidoHTML = `
     <div class="flex justify-between text-xl conteint">
@@ -117,15 +142,19 @@ const mostrarOperaciones = () => {
   $contenidoOPeraciones.innerHTML = contenidoHTML;
 };
 
-
 //// Funciones para manejar las categorías en localStorage
-const getCategorias = () => JSON.parse(localStorage.getItem("categorias")) || [];
-const setCategorias = (categorias) => localStorage.setItem("categorias", JSON.stringify(categorias));
+const getCategorias = () =>
+  JSON.parse(localStorage.getItem("categorias")) || [];
+const setCategorias = (categorias) =>
+  localStorage.setItem("categorias", JSON.stringify(categorias));
+
+// string -> cine, comida
+// objeto -> {nombre:cine,id:1}
 const addCategoria = (categoria) => {
-  const categorias = getCategorias();
-  categorias.push(categoria);
-  setCategorias(categorias);
+  setCategorias([...getCategorias(),categoria]);
 };
+
+//
 
 // // Función para actualizar el select de categorías
 // const actualizarSelectCategorias = () => {
@@ -150,7 +179,7 @@ const addCategoria = (categoria) => {
 
 //   // Obtener el valor del input
 //   const nuevaCategoria = event.target.querySelector("input[type='text']").value.trim();
-  
+
 //   if (nuevaCategoria !== "") {
 //     addCategoria(nuevaCategoria); // Agregar nueva categoría a localStorage
 //     actualizarSelectCategorias(); // Actualizar el select
@@ -163,7 +192,6 @@ const addCategoria = (categoria) => {
 //   actualizarSelectCategorias();
 // });
 
-
 // // const actualizarCategorias = ( ) => {
 
 // //   const categorias = getCategorias ();
@@ -172,6 +200,5 @@ const addCategoria = (categoria) => {
 // //     // Añadir cada categoría al contenedor
 // //     console.log(actualizarCategorias);
 // //     console.log(cetegorias);
-    
-  
+
 // // };
